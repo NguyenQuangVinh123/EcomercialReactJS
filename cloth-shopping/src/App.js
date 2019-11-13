dc1372a2b7b9fa2f41370021ff3a30e74cba0df7
Nguyễn Quang Vinh <31350719+NguyenQuangVinh123@users.noreply.github.com>
2019-11-13 22:18:18 +0700

    Route

--- cloth-shopping/src/App.js
+++ cloth-shopping/src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component'
+import {Switch,Route} from 'react-router-dom'
+import HomePage from './pages/homepage/homepage.component'
+
 function App() {
+
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path= '/' component = {HomePage} />
+        <Route exact path= '/topics' component = {TopicsList} />
+        <Route path='/topics/:topicId' component ={TopicDetail} />
+      </Switch>
     </div>
   );
+}
+const TopicDetail = () =>{
+  return(
+    <div>Topic detail</div>
+  )
+}
+const TopicsList = () =>{
+  return(
+    <div>Topic</div>
+  )
 }
 
 export default App;
