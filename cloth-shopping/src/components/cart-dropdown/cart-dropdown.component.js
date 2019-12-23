d08e1a99d5f643e2e2fc551e73b8ff75c47d682e
VinhNguyen <vinh1593570@gmail.com>
2019-12-23 10:15:22 +0700

    finish checkout

--- cloth-shopping/src/components/cart-dropdown/cart-dropdown.component.js
+++ cloth-shopping/src/components/cart-dropdown/cart-dropdown.component.js
@@ -1,24 +1,33 @@
-import React from 'react'
-import CartItem from '../cart-item/cart-item.component'
-import './cart-dropdown.styles.scss'
-import CustomButton from '../custom-button/custom-button.component'
-import { connect } from 'react-redux'
-import { selectCartItems} from '../../redux/cart/cart.selectors'
-import {createStructuredSelector} from 'reselect'
-const CartDropDown = ({cartItems}) => (
-    <div className= 'cart-dropdown'>
-        <div className='cart-items'>
-            <div className='cart-items'>
-                {
-                    cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
-                }
-            </div>
-            <CustomButton>GO TO CHECKOUT</CustomButton>
-        </div>
-        
+import React from "react";
+import CartItem from "../cart-item/cart-item.component";
+import "./cart-dropdown.styles.scss";
+import { withRouter } from "react-router-dom";
+import CustomButton from "../custom-button/custom-button.component";
+import { connect } from "react-redux";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
+import { createStructuredSelector } from "reselect";
+import { toogleCartHidden } from "../../redux/cart/cart.actions";
+const CartDropDown = ({ cartItems, history, dispatch }) => {
+    console.log(dispatch)
+  return (
+    <div className="cart-dropdown">
+      <div className="cart-items">
+        {cartItems.length ? (
+          cartItems.map(cartItem => (
+            <CartItem key={cartItem.id} item={cartItem} />
+          ))
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
+      </div>
+      <CustomButton onClick={() => { history.push("/checkout");
+      dispatch(toogleCartHidden())}}>
+        GO TO CHECKOUT
+      </CustomButton>
     </div>
-)
+  );
+};
 const mapStateToProps = createStructuredSelector({
-    cartItems : selectCartItems
-})
-export default connect(mapStateToProps) (CartDropDown);
+  cartItems: selectCartItems
+});
+export default withRouter(connect(mapStateToProps)(CartDropDown));
--- cloth-shopping/src/redux/cart/cart.selectors.js
+++ cloth-shopping/src/redux/cart/cart.selectors.js
@@ -19,4 +19,11 @@ export const selectCartItemsCount = createSelector(
     cartItems => 
         cartItems.reduce((accumaltedQuantity,cartItem) => accumaltedQuantity + cartItem.quantity
         ,0)
+);
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems => 
+    cartItems.reduce((accumaltedQuantity,cartItem) => accumaltedQuantity + cartItem.quantity * cartItem.price
+    ,0)
 )
